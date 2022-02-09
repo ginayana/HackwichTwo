78ce13c0e00081e13295070d2a8a05c02e3d20fc
ginayana <97510602+ginayana@users.noreply.github.com>
2022-02-08 22:54:53 -1000

    Corrections made to Assignment #3

--- HackwichTwo/secondViewController.swift
+++ HackwichTwo/secondViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class secondViewController: UIViewController {
 
-    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +21,12 @@ class secondViewController: UIViewController {
     
     
     @IBAction func changeColorButtonPressed(_ sender: Any)
-    {{var text: String?}
-    {var textColor: UIColor.green}
-        
-        
+    {
         //when button is pressed we want the background color to change to blue
         self.view.backgroundColor = UIColor.blue
-        
-        
+        firstLabel.text = "I created my first label and I've changed the text"
+        firstLabel.textColor = UIColor.white
+    
         
     }
 
